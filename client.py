815acbda43feb0ed5cdff07e5ad61c0db3a33c8d
Jorge Francisco de Lima Junior <jflj@WINDOWS.CIN.UFPE.BR>
2023-03-03 11:54:29 -0300

    Leitura do txt e conversão dos dados do estado

--- client.py
+++ client.py
@@ -1,7 +1,24 @@
 #Aqui vocês irão colocar seu algoritmo de aprendizado
 import connection as cn
 
-state = cn.connect()
+state = cn.connect(2037)
 
-estado, recompensa = cn.get_state_reward(state, "jump")
+acao = "right"
+estado, recompensa = cn.get_state_reward(state, acao)
 
+# Lendo o documento txt em formato de array
+# com cada linha ocupando uma posição
+f = open('resultado.txt', 'w')
+linhas = f.readlines()
+print(linhas[0])
+
+# conversão das informações recebidas do estado de binario para decimal
+# para calcular a posiçao na array
+direcao = int(estado[7:], 2) 
+print(int(estado[2:7], 2), direcao)
+
+# posicao = (int(estado[2:7], 2)-1)*4 + direcao
+
+
+
+f.close()
